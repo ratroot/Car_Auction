709dcbfe6849b0ac52821ba42b565d0c24f611b2
Qazi Abdur Rehman <qrehman@kpsoft.com.pk>
2020-11-14 23:21:50 +0500

    13NOV

--- database/migrations/2020_11_14_180117_update_bidding_field_latesBid_to_int.php
+++ database/migrations/2020_11_14_180117_update_bidding_field_latesBid_to_int.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateBiddingFieldToInt extends Migration
+class UpdateBiddingFieldLatesBidToInt extends Migration
 {
     /**
      * Run the migrations.
