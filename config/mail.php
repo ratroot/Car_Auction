86c1690e24fc791455e11c0f9a2b032c09a13f07
Qazi Abdur Rehman <qrehman@kpsoft.com.pk>
2020-12-02 11:10:13 +0500

    24NOV

--- config/mail.php
+++ config/mail.php
@@ -107,12 +107,6 @@ return [
         ],
     ],
 
-    'stream' => [
-        'ssl' => [
-            'allow_self_signed' => true,
-            'verify_peer'       => false,
-            'verify_peer_name'  => false,
-        ],
-    ],
+
 
 ];
